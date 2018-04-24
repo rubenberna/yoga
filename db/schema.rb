a51e83d29e40116be7eb079c154aa3feb769805e
ruben bernardes <rubenmbernardes@gmail.com>
2018-04-24 13:51:47 +0200

    associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424114632) do
+ActiveRecord::Schema.define(version: 20180424114940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "date"
+    t.bigint "user_id"
+    t.bigint "service_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["service_id"], name: "index_bookings_on_service_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "services", force: :cascade do |t|
     t.string "name"
@@ -41,4 +51,6 @@ ActiveRecord::Schema.define(version: 20180424114632) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "services"
+  add_foreign_key "bookings", "users"
 end
