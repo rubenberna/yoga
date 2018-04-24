651ddad545d4abc739c60e7d07a1858748d68d96
ruben bernardes <rubenmbernardes@gmail.com>
2018-04-24 15:57:28 +0200

    controller plus navbar start

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
+gem 'better_errors'
+gem 'binding_of_caller'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :bookings
+
+  validates :name, presence: true
 end
