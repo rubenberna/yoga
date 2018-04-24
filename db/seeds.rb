c99805ada433ae18f8976040056c2e44e2fbf734
ruben bernardes <rubenmbernardes@gmail.com>
2018-04-24 14:04:56 +0200

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  services = Service.new(name: Faker::FamilyGuy.character, description: Faker::Lorem.paragraph, photo: Faker::Fillmurray.image, price: rand(100))
+  services.save!
+end
